e086b62380d5b7fb2d31313b2f3561376ba249fc
yoonmisu <ymisu9@gmail.com>
2025-05-07 20:18:48 +0900

    탭 추가

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import streamlit as st
 
 col1, col2 = st.columns([2, 3])
+tab1, tab2 = st.tabs(['Tab A','Tab B'])
 with col1 :
     st.title("here is column1 title")
 
 with col2 :
     st.title("here is column2 title")
-    st.checkbox("this is checkbox1 in col2")
+    with tab1 :
+        st.write('hello')
+    with tab2 :
+        st.write('hi')
 
 
 col1.subheader("i am column1 subheader")
