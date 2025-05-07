034580d3affb8844f984605c0f728e7cc4136eb9
yoonmisu <ymisu9@gmail.com>
2025-05-07 20:10:36 +0900

    수정

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
-st.title('title')
-st.header('this is header')
-st.subheader('this is subheader')
+col1, col2 = st.columns([2, 3])
+with col1 :
+    st.title("here is column1 title")
+
+with col2 :
+    st.title("here is column2 title")
+    st.checkbox("this is checkbox1 in col2")
